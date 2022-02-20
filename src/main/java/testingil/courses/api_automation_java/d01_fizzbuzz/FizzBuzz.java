174f3d8924742c9c7837404bd782867e0e55163d
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-02-20 11:38:32 +0200

    clean up

--- src/main/java/testingil/courses/api_automation_java/d01_fizzbuzz/FizzBuzz.java
+++ src/main/java/testingil/courses/api_automation_java/d01_fizzbuzz/FizzBuzz.java
@@ -2,6 +2,6 @@ package testingil.courses.api_automation_java.d01_fizzbuzz;
 
 public class FizzBuzz {
     public String calculate(int i) {
-        return "Nothing yet!";
+        return "1";
     }
 }
