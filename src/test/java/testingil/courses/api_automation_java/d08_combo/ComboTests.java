9e1e0c663b8c916f7be80bcfc52f933f104c891d
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-02-20 11:32:39 +0200

    Added combo tests

---
+++ src/test/java/testingil/courses/api_automation_java/d08_combo/ComboTests.java
@@ -0,0 +1,26 @@
+package testingil.courses.api_automation_java.d08_combo;
+
+public class ComboTests {
+    // test 1
+    // load the beatles xml
+    // load the beatles json
+    // prove that paul plays bass in both of them
+
+    // test 2
+    // create a data object for the MCU
+    // MCU includes films
+    // Each film has a name and a series counter (Captain America: The Winter Soldier is 2)
+    // Each film has super-heroes in it (could be more than one)
+
+    // Create data objects
+    // Load the following data objects as follows:
+    // "Thor" -> Thor
+    // "Captain America - The First Avenger" -> Cap
+    // "Black Widow" -> Black Widow
+    // "Avengers" -> Thor, Cap, Black Widow, Hawekeye, Hulk, Iron Man
+    //
+    // Create the JSON String using ObjectMapper.writeValue()
+    // Prove that the JSON String contains the all the names of the super heroes
+    // Prove that Ant-Man was not in Avengers
+
+}
