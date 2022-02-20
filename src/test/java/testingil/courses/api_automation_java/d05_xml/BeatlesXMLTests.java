25abcaf36fc76bddbf190f7e65e28dafc6d7e3a2
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-02-20 13:04:19 +0200

    xml tests

--- src/test/java/testingil/courses/api_automation_java/d05_xml/BeatlesXMLTests.java
+++ src/test/java/testingil/courses/api_automation_java/d05_xml/BeatlesXMLTests.java
@@ -3,6 +3,7 @@ package testingil.courses.api_automation_java.d05_xml;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,20 +15,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BeatlesXMLTests {
 
     private Document doc;
+    private Element rootElement;
 
     @BeforeEach
     public void setup() throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         var db = dbf.newDocumentBuilder();
         doc = db.parse(new File(".//src//main//resources//Beatles.xml"));
+        rootElement = doc.getDocumentElement();
     }
 
     @Test
     public void there_are_four_artists(){
+        var artist_list = rootElement.getElementsByTagName("Artist");
+        assertEquals(4, artist_list.getLength());
     }
 
     @Test
     public void two_are_dead_and_two_are_alive(){
+        var artist_list = rootElement.getElementsByTagName("Artist");
+        int countAlive = 0;
+        int countDead=0;
+        for (int i=0; i< artist_list.getLength(); i++){
+            var artist = (Element) artist_list.item(i);
+            String isAliveText = artist.getElementsByTagName("IsAlive").item(0).getTextContent();
+            if (isAliveText.equals("Yes"))
+                countAlive++;
+            if (isAliveText.equals("No"))
+                countDead++;
+        }
+      //  assertEquals(2, countAlive);
+        assertEquals(2, countDead);
 
     }
 
