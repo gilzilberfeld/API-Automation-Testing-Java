139b04d2929107d09b92acd3c2052b3a9697a1c9
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-02-20 11:42:16 +0200

    fizzbuzz solution

--- src/main/java/testingil/courses/api_automation_java/d01_fizzbuzz/FizzBuzz.java
+++ src/main/java/testingil/courses/api_automation_java/d01_fizzbuzz/FizzBuzz.java
@@ -2,6 +2,12 @@ package testingil.courses.api_automation_java.d01_fizzbuzz;
 
 public class FizzBuzz {
     public String calculate(int i) {
-        return "1";
+        if (i % 15 == 0)
+            return "fizzbuzz";
+        if (i % 3 ==0)
+            return "fizz";
+        if (i % 5 ==0)
+            return "buzz";
+        return Integer.toString(i);
     }
 }
