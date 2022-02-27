d830471e91be26cd322ec1afdc0cb9ea32884af9
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-02-27 11:13:43 +0200

    added solutions for deserialization

--- src/test/java/testingil/courses/api_automation_java/d07_deserialization/JSONDeserializationTests.java
+++ src/test/java/testingil/courses/api_automation_java/d07_deserialization/JSONDeserializationTests.java
@@ -2,8 +2,10 @@ package testingil.courses.api_automation_java.d07_deserialization;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import testingil.courses.api_automation_java.d07.serialization.Artist;
 import testingil.courses.api_automation_java.d07.serialization.Root;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,12 +20,27 @@ public class JSONDeserializationTests {
     }
 
     @Test
-    public void two_are_dead_and_two_are_alive(){
-
+    public void two_are_dead_and_two_are_alive() throws Exception {
+        ObjectMapper om = new ObjectMapper();
+        Root root = om.readValue((Paths.get(".//src//main//resources//Beatles.JSON").toFile()), Root.class);
+        int isDeadCount=0;
+        int isAliveCount=0;
+        for (int i=0; i<root.beatles.artists.size(); i++){
+            Artist artist = root.beatles.artists.get(i);
+            if (artist.isAlive.equals("Yes"))
+                isAliveCount++;
+            if (artist.isAlive.equals("No"))
+                isDeadCount++;
+        }
+        assertEquals(2, isAliveCount);
+        assertEquals(2, isDeadCount);
     }
 
     @Test
-    public void ringo_plays_drums(){
-
+    public void ringo_plays_drums() throws Exception {
+        ObjectMapper om = new ObjectMapper();
+        Root root = om.readValue((Paths.get(".//src//main//resources//Beatles.JSON").toFile()), Root.class);
+        Artist ringo = root.beatles.artists.get(3);
+        assertEquals("Drums",ringo.plays);
     }
 }
