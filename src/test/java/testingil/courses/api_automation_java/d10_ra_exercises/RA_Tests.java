16af66a3b307ae9f944b6c8fe488e56dd0a9a4e0
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-03-04 12:38:55 +0200

    Updated exercise file

--- src/test/java/testingil/courses/api_automation_java/d10_ra_exercises/RA_Tests.java
+++ src/test/java/testingil/courses/api_automation_java/d10_ra_exercises/RA_Tests.java
@@ -10,24 +10,13 @@ import static org.hamcrest.Matchers.*;
 public class RA_Tests {
 
     // JsonPlaceHolder api
-    // use size() in the body to query the array size
+    // use body("size()") in the body to query the array size
     @Test
     public void number_of_total_posts_is_100(){
-        given().
-        when().
-                get("http://jsonplaceholder.typicode.com/posts").
-                then().
-                body("size()", equalTo(100));
     }
 
-    // JsonPlaceHolder api
     @Test
     public void title_of_first_post_for_userid_1_contains_facere(){
-        given().
-        when().
-                get("http://jsonplaceholder.typicode.com/posts/1").
-        then().
-                body("title", containsString("facere"));
     }
 
     // apichallenges api
@@ -39,26 +28,9 @@ public class RA_Tests {
                 "            \"description\": \"dognabbit\"\n" +
                 "        }";
 
-        int id =
-                given().
-                    body(request_body).
-                when().
-                    post("http://apichallenges.herokuapp.com/todos").
-                then().
-                   statusCode(201).
-                   extract().path("id");
-
-        given().
-                queryParam("id", id).
-        when().
-                get("http://apichallenges.herokuapp.com/todos").
-        then().
-                statusCode(200).
-                log().all().
-                body("todos[0].title", equalTo("My Todo"));
     }
 
-    // use hasItem to find if the ID was added.
+    // use body("Y", hasItem(X)); to find if the ID was added.
     @Test
     public void create_todo_and_check_if_added_to_todos(){
         String request_body="{\n" +
@@ -67,20 +39,6 @@ public class RA_Tests {
                 "            \"description\": \"dognabbit\"\n" +
                 "        }";
 
-        int id =
-                given().
-                        body(request_body).
-                        when().
-                post("http://apichallenges.herokuapp.com/todos").
-                then().
-                        statusCode(201).
-                        extract().path("id");
-
-        given().
-        when().
-                get("http://apichallenges.herokuapp.com/todos").
-        then().
-                body("todos.id", hasItem(id));
     }
 
     // use object for serialization
@@ -91,34 +49,6 @@ public class RA_Tests {
        new_todo.title = "new title";
        new_todo.description = "new description";
        new_todo.doneStatus = false;
-
-       int id =
-                given().
-                        body(new_todo).
-                when().
-                        post("http://apichallenges.herokuapp.com/todos").
-                then().
-                        statusCode(201).
-                        extract().path("id");
-
-       new_todo.doneStatus = true;
-
-        given().
-                pathParam("todoId", id).
-                contentType(ContentType.JSON).
-                body(new_todo).
-        when().
-                post("http://apichallenges.herokuapp.com/todos/{todoId}").
-        then().
-                statusCode(200).
-                body("doneStatus", is(true));
-
-        given().
-                pathParam("todoId", id).
-        when().
-                delete("http://apichallenges.herokuapp.com/todos/{todoId}").
-        then().
-                statusCode(200);
 
     }
 }
