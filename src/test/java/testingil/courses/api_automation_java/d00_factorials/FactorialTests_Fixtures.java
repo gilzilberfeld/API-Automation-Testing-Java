ca40fa8bdc09641f9b8f0a8db33ea26115bfb492
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-29 12:15:00 +0300

    added parameterized and fixture examples

--- src/test/java/testingil/courses/api_automation_java/d00_factorials/FactorialTests_Fixtures.java
+++ src/test/java/testingil/courses/api_automation_java/d00_factorials/FactorialTests_Fixtures.java
@@ -1,32 +1,36 @@
 package testingil.courses.api_automation_java.d00_factorials;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FactorialTests {
+public class FactorialTests_Fixtures {
+
+    private Factorial factorial;
+
+    @BeforeEach
+    public void setup(){
+        factorial = new Factorial();
+    }
+
     @Test
     public void factorial_tests() {
-        Factorial factorial = new Factorial();
         assertEquals(1, factorial.calculate(1));
         assertEquals(2, factorial.calculate(2));
         assertEquals(6, factorial.calculate(3));
-
     }
 
     @Disabled
     @Test
     public void negative_factorial() {
-        Factorial factorial = new Factorial();
         assertEquals(0, factorial.calculate(-3));
     }
 
     @Test
     public void big_factorial() {
-        Factorial factorial = new Factorial();
         assertEquals(3628800, factorial.calculate(10));
     }
 
 }
-
---
+++ src/test/java/testingil/courses/api_automation_java/d00_factorials/FactorialTests_Parameterized.java
@@ -0,0 +1,57 @@
+package testingil.courses.api_automation_java.d00_factorials;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FactorialTests_Parameterized {
+
+    @Disabled
+    @CsvSource({"1, 1",
+            "2, 2",
+            "3, 6",
+            "-3, 0",
+            "10, 3628800"})
+    @ParameterizedTest
+    public void factorial_tests_with_values(int input, int result) {
+        Factorial factorial = new Factorial();
+        assertEquals(result, Factorial.calculate(input));
+    }
+
+    //@Disabled
+    @MethodSource("factorial_values")
+    @ParameterizedTest
+    public void factorial_tests_with_method(int input, int result) {
+        Factorial factorial = new Factorial();
+        String property = Path.of("").toAbsolutePath().toString();
+        assertEquals(result, Factorial.calculate(input));
+
+    }
+
+    public static Stream<Arguments> factorial_values() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(2, 2),
+                Arguments.of(3, 6),
+                Arguments.of(-3, 0),
+                Arguments.of(10, 3628800)
+        );
+    }
+
+    @Disabled
+    @CsvFileSource(resources = "/Factorials.csv")
+    @ParameterizedTest
+    public void factorial_tests_with_file(int input, int result) {
+        Factorial factorial = new Factorial();
+
+        assertEquals(result, Factorial.calculate(input));
+    }
+}
