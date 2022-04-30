c1ca396cb96d941d4f7bb970e9793c2442b263fc
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-04-30 11:50:35 +0300

    Added code to the calculatordisplay in ex 2

--- src/main/java/testingil/courses/api_automation_java/d02_calculatordisplay/CalculatorDisplay.java
+++ src/main/java/testingil/courses/api_automation_java/d02_calculatordisplay/CalculatorDisplay.java
@@ -1,10 +1,54 @@
 package testingil.courses.api_automation_java.d02_calculatordisplay;
 
+
 public class CalculatorDisplay {
-    public void press(String s) {
+    String display = "";
+    int lastArgument = 0;
+    int result = 0;
+    Boolean newArgument = false;
+    Boolean shouldReset = true;
+
+    OperationType lastOperation;
+
+    public void press(String key) {
+        if (key.equals("+")) {
+            lastOperation = OperationType.Plus;
+            lastArgument = Integer.parseInt(display);
+            newArgument = true;
+        } else {
+            if (key.equals("/")) {
+                lastOperation = OperationType.Div;
+                lastArgument = Integer.parseInt(display);
+                newArgument = true;
+            } else if (key.equals("=")) {
+                int currentArgument = Integer.parseInt(display);
+                if (lastOperation == OperationType.Plus) {
+                    display = Integer.toString(lastArgument + currentArgument);
+                }
+                if (lastOperation == OperationType.Div && currentArgument == 0) {
+                    display = "Division By Zero Error";
+                }
+                shouldReset = true;
+            } else {
+                if (shouldReset) {
+                    display = "";
+                    shouldReset = false;
+                }
+                if (newArgument) {
+                    display = "";
+                    newArgument = false;
+                }
+                display += key;
+            }
+        }
     }
 
     public String getDisplay() {
-        return "0";
+        if (display.equals(""))
+            return "0";
+        if (display.length() > 5)
+            return "E";
+        return display;
     }
+
 }
---
+++ src/main/java/testingil/courses/api_automation_java/d02_calculatordisplay/OperationType.java
@@ -0,0 +1,6 @@
+package testingil.courses.api_automation_java.d02_calculatordisplay;
+
+public enum OperationType {
+    Plus,
+    Div
+}
